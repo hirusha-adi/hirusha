a74a4c6c7167141fb510297bcdc2565eb1e7da40
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-09-28 15:46:26 +0530

    remove everything

--- js/custom.js
+++
@@ -1,4 +0,0 @@
-console.log(
-    "%c.       . .     \n|       | |     \n|-. ,-. | | ,-. \n| | |-' | | | | \n' ' `-' ' ' `-'  \nLooking at the Source Code? \nIts all available in my GitHub at https://github.com/hirusha-adi/hirusha",
-    'color: cyan'
-);
